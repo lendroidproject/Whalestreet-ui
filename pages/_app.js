e8b32d0af8ebefffad8c3418025c39280aa98544
alonavns <alona.vns@outlook.com>
2020-12-03 13:22:30 +0200

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -115,7 +115,7 @@ class App extends NextApp {
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KJNBZPW');`,
+                })(window,document,'script','dataLayer','G-CEVZQESQGT');`,
               }}
             />
           )}
