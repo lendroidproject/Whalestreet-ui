656521179ba435d55b216023cba73d360ceb2128
alonavns <alona.vns@outlook.com>
2021-01-23 16:43:05 +0200

    ifura

--- layouts/Account.js
+++ layouts/Account.js
@@ -166,7 +166,7 @@ const providerOptions = (network) => ({
   walletconnect: {
     package: WalletConnectProvider,
     options: {
-      // infuraId: infuras[network],
+      infuraId: infuras[network],
     },
   },
   // fortmatic: {
